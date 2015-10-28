5bd85401ff8beaa054498e520591072e0eb6fd73
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-10-28 16:44:27 -0200

    adicionado suporte a postgres no slave

--- mesos-slave/s3_worker.py
+++ mesos-slave/s3_worker.py
@@ -11,10 +11,19 @@ os.environ["AWS_SECRET_ACCESS_KEY"]= sys.argv[3]
 os.environ["BUCKET_NAME"]=sys.argv[4]
 os.environ["AWS_DEFAULT_REGION"]=sys.argv[5]
 
+
+
+
+
 def main():
-    print('download init')
-    #part1 get file from s3 bucket
-    __exec = call(['aws','s3','cp',str('s3://' + os.environ['BUCKET_NAME'] + '/' + sys.argv[1]),'.'])
+    if (sys.argv[6] != 'upload'):
+        print('upload init')
+        #part1 get file from s3 bucket
+        __exec = call(['aws','s3','cp',sys.argv[1], str('s3://' + os.environ['BUCKET_NAME'] + '/' + sys.argv[1]) ])
+    else:
+        print('download init')
+        #part1 get file from s3 bucket
+        __exec = call(['aws','s3','cp',str('s3://' + os.environ['BUCKET_NAME'] + '/' + sys.argv[1]),'.'])
 
 if __name__ == "__main__":
     main()
